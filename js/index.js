4b04ff58937f88762efb2e18e56b53bcc3bfa447
Serg Klukvin <klukvin@ukr.net>
2020-05-20 13:48:39 +0300

    end

---
+++ js/index.js
@@ -0,0 +1,56 @@
+let tmp = 0;
+let temp = 0;
+let tmpTwo = 0;
+let tempThree = 0;
+let tmpFour = 0;
+
+function myActionOne() {
+  console.log(event);
+  if (tmp === 0) {
+    localCInner.style.display = "block";
+    localDInner.style.display = "block";
+    localEInner.style.display = "block";
+    tmp = 1;
+  } else if (tmp === 1) {
+    localCInner.style.display = "none";
+    localDInner.style.display = "none";
+    localEInner.style.display = "none";
+    localCInnerList.style.display = "none";
+    localDInnerList.style.display = "none";
+    localEInnerList.style.display = "none";
+    tmp = 0;
+  }
+}
+
+function myActionTwo() {
+  console.log(event);
+  if (tmpTwo === 0) {
+    localCInnerList.style.display = "block";
+    tmpTwo = 1;
+  } else if (tmpTwo === 1) {
+    localCInnerList.style.display = "none";
+    tmpTwo = 0;
+  }
+}
+
+function myActionThree() {
+  console.log(event);
+  if (tempThree === 0) {
+    localDInnerList.style.display = "block";
+    tempThree = 1;
+  } else if (tempThree === 1) {
+    localDInnerList.style.display = "none";
+    tempThree = 0;
+  }
+}
+
+function myActionFour() {
+  console.log(event);
+  if (tmpFour === 0) {
+    localEInnerList.style.display = "block";
+    tmpFour = 1;
+  } else if (tmpFour === 1) {
+    localEInnerList.style.display = "none";
+    tmpFour = 0;
+  }
+}
